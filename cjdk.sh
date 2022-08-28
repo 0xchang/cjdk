7db2786c5aeada8a8b234ce2c6cf6c7f79223977
0xchang <93780473+0xchang@users.noreply.github.com>
2022-08-28 12:22:32 +0800

    Update cjdk.sh

--- cjdk.sh
+++ cjdk.sh
@@ -9,7 +9,11 @@ PATH=`cat /tmp/cjdk.config.txt`
 fi
 
 JAVA_HOME=/usr/share/Java/Java
+if [ "$1" == "" ]; then
 read -p 'Input you want java version: ' Java_Version
+else
+Java_Version=$1
+fi
 Java_Bin=$JAVA_HOME$Java_Version/bin
 export PATH=$Java_Bin:$PATH
 
